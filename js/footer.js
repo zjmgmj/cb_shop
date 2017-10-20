aa739b765986ea2322f8433a0351f7a9286d0ecf
zhjm <943257156@qq.com>
2017-10-20 12:24:17 +0800

    20171020

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+$(function(){
+	var a='<a href="/cb_kj/index.html"><p class="menu-icon"><img src="/cb_kj/images/storebrand.png" /></p><p class="menu-text">品牌商家</p></a><a href="/cb_kj/tmpl/articlelist.html"><p class="menu-icon"><img src="/cb_kj/images/info.png" /></p><p class="menu-text">情报</p></a><a href="/cb_kj/tmpl/shangschool.html"><p class="menu-icon"><img src="/cb_kj/images/school.png" /></p><p class="menu-text">商学院</p></a><a href="/cb_kj/tmpl/member.html"><p class="menu-icon"><img src="/cb_kj/images/user.png" /></p><p class="menu-text">我</p></a>';
+	$(".menu").html(a);
+})
